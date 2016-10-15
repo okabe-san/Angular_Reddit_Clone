ad5fdfbc1b1bd974b927e222e1ee4441a082fead
Akiko Okabe <aki0u0@gmail.com>
2016-10-15 14:15:48 -0600

    add initial content creater function

---
+++ controllers/create.content.js
@@ -0,0 +1,42 @@
+(function () {
+  'use strict';
+
+  var app = angular.module('RedditCloneApp')
+    .controller('CreateContentController', CreateContentController);
+
+  function CreateContentController ($rootScope) {
+    $rootScope.id = 1;
+
+    $rootScope.brewingArr = [{
+      id: $rootScope.id++,
+      image: 'BaereBrewing.jpeg',
+      title: 'Baere Brewing',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+      author: 'Apple',
+      rating: 4.5,
+      date: '10-10-2016',
+      reviews: [{
+        id: $rootScope.id++,
+        name: 'Pie',
+        comment: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
+      }]
+    }, {
+      id: $rootScope.id++,
+      iamge: 'TRVE.jpeg',
+      title: 'TRVE Brewing Company',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+      author: 'Darkest',
+      rating: 4.3,
+      date: '10-16-2016',
+      reviews: [{
+          id: $rootScope.id++,
+          name: 'Syn',
+          comment: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
+        },  {
+          id: $rootScope.id++,
+          name: 'Angel',
+          comment: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
+        }]
+      }]
+    }
+})()
