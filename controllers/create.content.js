d6ac0988b8bb7a643fb6fed8226b0eb11a0adf1d
Akiko Okabe <aki0u0@gmail.com>
2016-10-15 14:46:57 -0600

    add initial contents

--- controllers/create.content.js
+++ controllers/create.content.js
@@ -13,7 +13,7 @@
       title: 'Baere Brewing',
       description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
       author: 'Apple',
-      rating: 4.5,
+      rating: 5,
       date: '10-10-2016',
       reviews: [{
         id: $rootScope.id++,
@@ -22,11 +22,11 @@
       }]
     }, {
       id: $rootScope.id++,
-      iamge: 'TRVE.jpeg',
+      image: 'TRVE.jpeg',
       title: 'TRVE Brewing Company',
       description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
       author: 'Darkest',
-      rating: 4.3,
+      rating: 4,
       date: '10-16-2016',
       reviews: [{
           id: $rootScope.id++,
