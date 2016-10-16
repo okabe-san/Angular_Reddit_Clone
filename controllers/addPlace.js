20fbeef773226f6718ce29cab071ce3cc99e58bf
Akiko Okabe <aki0u0@gmail.com>
2016-10-16 15:16:10 -0600

    add sort by filter

--- controllers/addPlace.js
+++ controllers/addPlace.js
@@ -5,11 +5,21 @@
     .controller('addPlaceController', addPlaceController);
 
   function addPlaceController ($rootScope) {
+    // console.log($rootScope);
     $rootScope.brewingArr = [];
     this.add = (info) => {
       $rootScope.brewingArr.push({
         title: info.title
       });
     };
+
+    this.addComment = (place, comment) => {
+      // console.log(place, comment);
+      place.reviews.push({
+        name: comment.name,
+        comment: comment.comment
+      });
+    };
   }
+
 })()
